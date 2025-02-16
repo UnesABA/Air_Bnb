55ff7d09a36ad3cfdf0f1235c2de87dfb057e23e
UnesABA <unesaitbenali@gmail.com>
2025-02-16 10:17:31 +0100

    some css adjustement

--- Air_Bnb/src/components/Main.jsx
+++ Air_Bnb/src/components/Main.jsx
@@ -11,9 +11,9 @@ const Main = () => {
 
         <div className="online-experiences">
           <h1>Online Experiences</h1>
-          <h3 className= "gray-text">Join unique interactive activities led by</h3>
-          <h3 className= "gray-text">One-of-a-kind-hosts- all without leaving</h3>
-          <h3 className= "gray-text">home.</h3>
+          <h3>Join unique interactive activities led by</h3>
+          <h3>One-of-a-kind-hosts- all without leaving</h3>
+          <h3>home.</h3>
         </div>
       </div>
       
